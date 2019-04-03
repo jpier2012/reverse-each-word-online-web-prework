74dc71b3d6b8ca422bfeea4cff57a116b7b06cb9
James Pier <jpier2012@gmail.com>
2019-04-03 18:39:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word string
   array = string.split(' ')
   array.collect {|x| x.reverse}
   new_string = array*" "
+  puts new_string
 end
 
 reverse_each_word "This is a test string!"

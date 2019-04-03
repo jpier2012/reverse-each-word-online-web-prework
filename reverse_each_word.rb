a757dad868777fade498ec4d6a33212c1a3a0d33
James Pier <jpier2012@gmail.com>
2019-04-03 18:46:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,4 @@ def reverse_each_word string
   new_string = string.split(' ').collect {|x| x.reverse}*" "
 end
 
-reverse_each_word "This is a test string!"
+puts reverse_each_word "This is a test string!"
